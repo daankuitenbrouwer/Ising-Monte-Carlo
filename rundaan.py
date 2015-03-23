b394647801551a4d9bf6be8550c1cfa0184220f3
Daan Kuitenbrouwer <daankuitenbrouwer@gmail.com>
2015-03-23 18:30:48 +0100

    working cluster builder, still to make varying over different T. Ask jose whether method with not walking back is allowed

--- rundaan.py
+++ rundaan.py
@@ -7,12 +7,13 @@ import functionsdaan
 import matplotlib.pyplot as plt
 
 
-size =20 #Width of the lattice
-init_M = 0.99 #initial magnetization
-lattice = functions.initialize(size,init_M) #initialize the girl, M sets magnetization
+size =6 #Width of the lattice. The lattice should always be square, otherwise problems occur in de periodic bc definition.
+init_M = 1 #initial magnetization
+lattice = functionsdaan.initializelat(size,init_M) #initialize the girl, M sets magnetization
 while np.average(lattice) != init_M:
-    lattice =functions.initialize(size,init_M)
-T = 0.4  #temperature, entropy vs potential energy
+    lattice =functionsdaan.initializelat(size,init_M)
+blacklist,futurelattice = functionsdaan.initializeelse(size)
+T = 0.8  #temperature, entropy vs potential energy
 invT = 1./T
 beta = invT
 runtime = 5000 #Total time of the simulation
@@ -20,10 +21,8 @@ B = 0.1 #The favorite orientation
 Trange = np.linspace(0.001,1,5)
 
 #Initializing arrays for clusterflippin'
-blacklist = np.zeros((len(lattice),len(lattice[0])))
-futurelattice =np.zeros((len(lattice),len(lattice[0])))
-xval = 5 #np.random.randint(0,len(lattice)+1)
-yval = 5 #np.random.randint(0,len(lattice)+1)
+xval = 1 #np.random.randint(0,len(lattice)+1)
+yval = 1 #np.random.randint(0,len(lattice)+1)
 blacklist[yval][xval] = 1
 lattice[yval][xval] = -1*lattice[yval][xval] #the module about accepting or rejecting the first randomly picked spin must be built still
 futurelattice[yval][xval] = lattice[yval][xval]
@@ -31,4 +30,6 @@ comp_xval = xval + 1
 comp_yval = yval
 blacklist[comp_yval][comp_xval] = 1
 cnt = 0
+print len(lattice)
 futurelattice = functionsdaan.wolff(lattice,blacklist,futurelattice,xval,yval,comp_xval,comp_yval,cnt,beta)
+print futurelattice,'=futurelattice'
